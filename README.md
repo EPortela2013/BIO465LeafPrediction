a9a668871c0bf12356f30732b5cdbe8cf2ccdcc4
Edward Andrey Portela Portela <EPortela@byu.edu>
2018-04-04 10:05:34 -0600

    Initial commit
    Former-commit-id: 86bdf25669e796b518d82abf4e36953b4e70899b

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BIO465LeafPrediction
 Code necessary to predict plant-disease pair based on image of leaf.
+
+Image(s) to be predicted need to be placed inside the ```images```
+folder. Also the ```classify.py``` script needs to be modified to point the script to the new image as of right now.
+
+All requirements for Caffe should be installed (requirements.txt is included inside ```caffe/distribute/python```)
+
+Another possibilty is to use one of the Dockers defined in ```caffe/docker```)
---
+++ classify.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import sys
+sys.path.append('caffe/distribute/python')
+
+import caffe
+
+caffe.set_device(0)
+caffe.set_mode_cpu()
+
+net = caffe.Net('deploy.prototxt',
+                'snapshots/snapshots__iter_52867.caffemodel',
+                caffe.TEST)
+
+#convert mean.binaryproto to numpy array
+blob = caffe.proto.caffe_pb2.BlobProto()
+data = open( 'mean_image/mean.binaryproto' , 'rb' ).read()
+blob.ParseFromString(data)
+arr = np.array( caffe.io.blobproto_to_array(blob) )
+
+# load input and configure preprocessing
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', arr[0].mean(1).mean(1))
+transformer.set_transpose('data', (2,0,1))
+transformer.set_channel_swap('data', (2,1,0))
+transformer.set_raw_scale('data', 255.0)
+
+
+#load the image in the data layer
+im = caffe.io.load_image('images/plantleaf2.png')
+net.blobs['data'].data[...] = transformer.preprocess('data', im)
+#compute
+out = net.forward()
+
+# other possibility : out = net.forward_all(data=np.asarray([transformer.preprocess('data', im)]))
+
+
+#print predicted labels
+labels = np.loadtxt("labels.txt", str, delimiter='\t')
+top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+
+#print(net.blobs['prob'].data[0].flatten().argsort())
+
+
+for x in labels[top_k]:
+    print (x)
